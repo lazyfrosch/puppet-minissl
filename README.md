bab0b583f47b230e2424b966858ed703223cdf90
Markus Frosch <markus@lazyfrosch.de>
2015-01-29 10:11:41 +0100

    Import into GIT

---
+++ README.md
@@ -0,0 +1,12 @@
+Puppet module minissl for Vagrant boxes
+=======================================
+
+Setup limit SSL certs into a Vagrant box.
+
+These certs are VERY INSECURE, because they are available online!
+
+Only use this in Vagrant boxes, where they are hidden.
+
+# Author
+
+Markus Frosch <markus@lazyfrosch.de>
---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+# == Class: minissl
+#
+# Setup limit SSL certs into a Vagrant box.
+#
+# These certs are VERY INSECURE, because they are available online!
+#
+# Only use this in Vagrant boxes, where they are hidden.
+#
+# === Author
+#
+# Markus Frosch <markus@lazyfrosch.de>
+#
+class minissl {
+
+  File {
+    ensure => file,
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0644',
+  }
+
+  # setup ssl on the client
+  file { "${settings::vardir}/ssl/certs/ca.pem":
+    source => 'puppet:///modules/minissl/ssl/certs/ca.pem',
+  }
+
+  file { "${settings::vardir}/ssl/certs/${::fqdn}.pem":
+    source => "puppet:///modules/minissl/ssl/certs/${::fqdn}.pem",
+  }
+
+  file { "${settings::vardir}/ssl/public_keys/${::fqdn}.pem":
+    source => "puppet:///modules/minissl/ssl/public_keys/${::fqdn}.pem",
+  }
+
+  file { "${settings::vardir}/ssl/private_keys/${::fqdn}.pem":
+    source => "puppet:///modules/minissl/ssl/private_keys/${::fqdn}.pem",
+    mode   => '0640',
+  }
+
+}
